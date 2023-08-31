1259bf391bfc51e93947c10fbd004c4316c72b18
Mohan <subramaniyanmohan23@gmail.com>
2023-08-31 23:58:08 +0530

    end of day

---
+++ PrepInsta/src/numbers_game/PrimeWithRange.java
@@ -0,0 +1,23 @@
+package numbers_game;
+
+public class PrimeWithRange {
+	
+		public static void main(String args[]){
+			int l=10,u=20;
+			for(int i=l ; i<=u ; i++){
+				if(prm(i))
+					System.out.println(i);
+			}
+		}
+		static boolean prm (int n){
+			if(n<2)
+				return false;
+			for(int i=2;i<=n/2;i++)
+				if(n%i==0){
+					return false;
+				}
+			return true;
+			
+		}
+	
+}
