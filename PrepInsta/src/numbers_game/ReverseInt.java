4719ef79e346626da8b5baddeba3f2eb13b97ca2
Mohan <subramaniyanmohan23@gmail.com>
2023-09-03 20:00:21 +0530

    new one

--- PrepInsta/src/numbers_game/ReverseInt.java
+++ PrepInsta/src/numbers_game/ReverseInt.java
@@ -5,11 +5,15 @@ public class ReverseInt {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		int num=12345, a=0, rem;
-		while(num!=0) {
-			rem=num%10;   //12345 becomes 5 , 4, 3, 2, 1
-			a=a*10+rem;	// 0=0*10+5=5 ,5=5*10+4=54 , 54=540+3 , 543=5430+2, 5432
-						// 
-			num/=10;	//1234  123  12 1
+//		while(num!=0) {
+//			rem=num%10;   //12345 becomes 5 , 4, 3, 2, 1
+//			a=a*10+rem;	// 0=0*10+5=5 ,5=5*10+4=54 , 54=540+3 , 543=5430+2, 5432
+//						// 
+//			num/=10;	//1234  123  12 1
+//		}
+		for(;num!=0;num/=10) {
+			rem=num%10;
+			a*=10+(rem);
 		}
 		System.out.println(a);
 	}
