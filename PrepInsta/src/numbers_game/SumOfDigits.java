e367eaf2c78e015cda094d077bf72b478fb51c11
Mohan <subramaniyanmohan23@gmail.com>
2023-09-03 16:57:30 +0530

    SumOfDigit addition

--- PrepInsta/src/numbers_game/SumOfDigits.java
+++ PrepInsta/src/numbers_game/SumOfDigits.java
@@ -4,7 +4,12 @@ public class SumOfDigits {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int num = 123456789, sum =0;
+		while(num!=0) {
+			sum+=num%10;
+			num=num/10;
+		}
+		System.out.println(sum +" "+num);
 	}
 
 }
