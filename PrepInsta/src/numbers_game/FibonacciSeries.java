3d86d15ef58c41a2c2e20ebd90b88cc6885c065f
MohanSubramaniyan <subramaniyanmohan23@gmail.com>
2023-09-09 16:27:42 +0530

    nothig special

--- PrepInsta/src/numbers_game/FibonacciSeries.java
+++ PrepInsta/src/numbers_game/FibonacciSeries.java
@@ -4,8 +4,8 @@ public class FibonacciSeries {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int num=10,a=0,b=1,i;
-		for(i=1;i<30;i++) {
+		int num=15,a=0,b=1,i;
+		for(i=0;i<=num;i++) {
 			int nxt=a+b;
 			a=b;
 			b=nxt;
