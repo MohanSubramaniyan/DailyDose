7675da69e420477ebe35162e04e6ac36fa2095af
MohanSubramaniyan <subramaniyanmohan23@gmail.com>
2023-09-09 16:22:47 +0530

    done

--- PrepInsta/src/numbers_game/FactorialOfNum.java
+++ PrepInsta/src/numbers_game/FactorialOfNum.java
@@ -4,10 +4,11 @@ public class FactorialOfNum {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-//		int num=5,i,res=1;
-//		for(i=2;i<=num;i++) {
-//			res*=i;
-//		}System.out.println(res);
-		
+		int num = 5, i, res = 1;
+		for (i = 2; i <= num; i++) {
+			res *= i;
+		}
+		System.out.println(res);
 
+	}
 }
---
+++ PrepInsta/src/numbers_game/PrimeFactorsOfNumber.java
@@ -0,0 +1,17 @@
+package numbers_game;
+
+public class PrimeFactorsOfNumber {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num=40;
+		for(int i=1;i<=num;i++) {
+			if(num%i==0) {
+				for(int j=2;j<=i;j++) {
+					
+				}
+			}
+		}
+	}
+
+}
