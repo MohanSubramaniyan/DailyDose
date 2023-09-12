af05333f1fe9ce7a62c0f3f0c36e07dfbbfbe355
MohanSubramaniyan <subramaniyanmohan23@gmail.com>
2023-09-12 17:41:33 +0530

    done with confused one

--- PrepInsta/src/arrays/SortAsec.java
+++ PrepInsta/src/arrays/SortAsec.java
@@ -1,13 +1,25 @@
-package arrays;
+package arrays ;
 
-public class SortAsec {
-
+class SortAsec{
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		int arr[]= {23,23,425,32,5456,82,-24,343,5};
-		for(int i=0;i<arr.length;i++) {
-			for(int j=i+1)
+		int n, i,j,temp, arr[]= {40,2,342,-32,-0,24};
+		n=arr.length;
+		for(i=0;i<n;i++) {
+			for(j=i+1;j<n;j++) {
+				if(arr[j]<arr[i]) {
+					temp=arr[i];
+					arr[i]=arr[j];
+					arr[j]=temp;
+				}
+			}
 		}
+		System.out.print("[");
+		for(int x:arr) {
+			System.out.print(x);
+			if(x!=arr[arr.length-1]) {
+				System.out.print(",");
+			}
+		}
+		System.out.print("]");
 	}
-
 }
