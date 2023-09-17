59788b6c1ab367fd8c437567f2a6aa47b94fe540
MohanSubramaniyan <subramaniyanmohan23@gmail.com>
2023-09-17 12:27:09 +0530

    jj

--- PrepInsta/src/arrays/SortingByTwoHalf.java
+++ PrepInsta/src/arrays/SortingByTwoHalf.java
@@ -1,10 +1,18 @@
 package arrays;
 
+import java.util.Arrays;
+
 public class SortingByTwoHalf {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int arr[] = { 3, 53, 67, 47, 4, 34, 6, 4, 3, 54, 54, 54 };
+		int size = arr.length;
+		
+		Sort(arr,size);
 	}
-
+	static void Sort(int arr[], int size) {
+		int temp;
+		for(int i=0;i<size-1)
+	}
 }
